41a6208cc38d8a98143a27a4c87525b4bca910fc
Sachin Sharma <sachin@gryt.fit>
2022-09-17 11:49:18 +0530

    FINAL: 1

--- components/homepage/TeamMembers.js
+++ components/homepage/TeamMembers.js
@@ -3,13 +3,14 @@ import gsap from "gsap";
 
 const teamMembers = [
   {
-    imageUrl: '../../static/members/member1.png'
+    imageUrl: '../../static/members/member1.png',
   },
   {
     imageUrl: '../../static/members/member2.png'
   },
   {
-    imageUrl: '../../static/members/member3.png'
+    imageUrl: '../../static/members/member3.png',
+    imgStyle: 'firstImage'
   },
   {
     imageUrl: '../../static/members/member1.png'
@@ -23,47 +24,54 @@ const teamMembers = [
 ]
 
 export default function TeamMembers() {
+
   useEffect(() => {
     var bannerControllerVideo = new ScrollMagic.Controller();
     var tween = new TimelineMax().to(".image-groups-items", 1, {
       borderRadius: 0,
-      scale: 1
+      scale: 1.2
     });
-
     // build scene
     new ScrollMagic.Scene({
-      triggerElement: "#trigger_element2",
-      triggerHook: "onEnter",
+      triggerElement: "#pinContainerPortfolio",
+      triggerHook: 0.8,
       duration: 700,
     })
       .setTween(tween)
       .addTo(bannerControllerVideo)
     // .addIndicators();
+  }, [])
 
-
+  useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
-    let sections = gsap.utils.toArray(".panel");
+    let sections = gsap.utils.toArray('.mamber-img');
+
     gsap.to(sections, {
-      xPercent: -35 * (sections.length + 1),
-      ease: "none",
+      xPercent: -60 * (sections.length - 1),
+      ease: 'ease-in',
       scrollTrigger: {
-        trigger: ".image-groups-section",
+        trigger: '#pinContainerPortfolio',
         pin: true,
-        scrub: 1,
-        end: "+=2500",
-      }
+        scrub: true,
+        start: "top 85px",
+        // markers: true,
+        end: "+=350%",
+      },
     });
   }, [])
 
+
+
   return (
     <section id="pinContainerPortfolio" className='image-groups-section'>
-      <div id="trigger_element2" className="spacer s0" />
       <div className="image-groups-items">
-        {teamMembers.map((item, index) => (
-          <div className="member-image panel" key={index}>
-            <img src={item.imageUrl} alt="" className='mamber-img' />
-          </div>
-        ))}
+        <div id="container" className='d-flex'>
+          {teamMembers.map((item, index) => (
+            <div className="member-image panel" key={index}>
+              <img src={item.imageUrl} alt="" className={`mamber-img ${item.imgStyle}`} />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   )
